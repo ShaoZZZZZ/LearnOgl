45bf2cabae2a32ddbf0fdfe7e6d58fac5b2961ea
Wu Zheng <OldWuMan@163.com>
2023-08-19 22:50:44 +0800

    Draw ten cubes
    第一章完结

--- OpenGL5.0/main.cpp
+++ OpenGL5.0/main.cpp
@@ -3,6 +3,11 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 #include "Shader.h"//<>是系统库，“”是用户自定库
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 void processInput(GLFWwindow* window);
 
 
@@ -34,24 +39,79 @@ int main() {
 
 	//设置视口Viewport
 	glViewport(0, 0, 800, 600);
-	glEnable(GL_CULL_FACE);//启动面剔除
-	glCullFace(GL_BACK);//剔除背面
+	//glEnable(GL_CULL_FACE);//启动面剔除
+	//glCullFace(GL_BACK);//剔除背面
 	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);//线框模式
-
+	glEnable(GL_DEPTH_TEST);//打开深度缓存
 	//顶点数组
+	//float vertices[] = {
+	//	//     ---- 位置 ----       ---- 颜色 ----     - 纹理坐标 -
+	//		 0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,   // 右上
+	//		 0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,   // 右下
+	//		-0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // 左下
+	//		-0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f    // 左上
+	//};
 	float vertices[] = {
-	-0.5f,-0.5f,0.0f,   1.0f,0,0, // 0
-	0.5f,-0.5f,0.0f,    0,1.0f,0, // 1
-	0.0f,0.5f,0.0f,     0,0,1.0f, // 2
-	//0.5f,-0.5f,0.0f,
-	//0.0f,0.5f,0.0f,
-	0.8f,0.8f,0.0f,	  0.2f,0.3f,0.6f// 3
-	};//三角形的三个点是逆时针画出来的
+	-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+	 0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
+	 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+	 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+	-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+	-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
 
+	-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+	 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+	 0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+	 0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+	-0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
+	-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+
+	-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+	-0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+	-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+	-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+	-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+	-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+	 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+	 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+	 0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+	 0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+	 0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+	 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+	-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+	 0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
+	 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+	 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+	-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+	-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+
+	-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+	 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+	 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+	 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+	-0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
+	-0.5f,  0.5f, -0.5f,  0.0f, 1.0f
+	};
 	unsigned int indices[] = {
 		0,1,2,
-		2,1,3
+		2,3,0
 	};
+	
+	glm::vec3 cubePositions[] = {
+  glm::vec3(0.0f,  0.0f,  0.0f),
+  glm::vec3(2.0f,  5.0f, -15.0f),
+  glm::vec3(-1.5f, -2.2f, -2.5f),
+  glm::vec3(-3.8f, -2.0f, -12.3f),
+  glm::vec3(2.4f, -0.4f, -3.5f),
+  glm::vec3(-1.7f,  3.0f, -7.5f),
+  glm::vec3(1.3f, -2.0f, -2.5f),
+  glm::vec3(1.5f,  2.0f, -2.5f),
+  glm::vec3(1.5f,  0.2f, -1.5f),
+  glm::vec3(-1.3f,  1.0f, -1.5f)
+	};
+
 	//创建VAO，VBO
 	unsigned int VAO;
 	glGenVertexArrays(1, &VAO);
@@ -69,36 +129,109 @@ int main() {
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
 	
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
 	//0号栏位，每三个栏位当成一个资料，不必规范化，每隔3个f长度读取下一个，该属性偏移量为0
 	glEnableVertexAttribArray(0);
 	//挖颜色值
-	glVertexAttribPointer(7, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
-	glEnableVertexAttribArray(7);
+	//glVertexAttribPointer(7, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+	//glEnableVertexAttribArray(7);
+	//UV值
+	glVertexAttribPointer(8, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(8);
+	
+	stbi_set_flip_vertically_on_load(true);//翻转纹理
+	//创建texture 缓存
+	unsigned int TexBufferA;
+	glGenTextures(1, &TexBufferA);
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_2D, TexBufferA);
+	int width, height,nrChannel;
+	unsigned char* data = stbi_load("container.jpg", &width, &height, &nrChannel,0);
+	if (data) {
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE,data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+	}
+	else {
+		std::cout << "load image failed." << std::endl;
+	}
+	//建第二个
+	unsigned int TexBufferB;
+	glGenTextures(1, &TexBufferB);
+	glActiveTexture(GL_TEXTURE3);
+	glBindTexture(GL_TEXTURE_2D, TexBufferB);
+	unsigned char* data2 = stbi_load("awesomeface.png", &width, &height, &nrChannel, 0);
+	if (data) {
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data2);
+		glGenerateMipmap(GL_TEXTURE_2D);
+	}
+	else {
+		std::cout << "load image failed." << std::endl;
+	}
 
+	stbi_image_free(data);
+	stbi_image_free(data2);
+
+	// calculate our transformation matrix here 
+
+	glm::mat4 trans;
+	//trans = glm::translate(trans, glm::vec3(0.5f, -0.5f, 0));//位移
+	//trans = glm::rotate(trans, (float)glfwGetTime(), glm::vec3(0, 0, 1.0f));//旋转
+	//trans = glm::scale(trans, glm::vec3(2.0f, 2.0f, 2.0f));//缩放
+	//tips：先缩放，再旋转，最后位移，否则会相互影响，根据矩阵左乘，故上面的顺序相反
+	glm::mat4 modelMat;
+	modelMat = glm::rotate(modelMat, glm::radians(-55.0f), glm::vec3(1.0f, 0, 0));
+	glm::mat4 viewMat;
+	viewMat = glm::translate(viewMat, glm::vec3(0, 0, -3.0f));
+	glm::mat4 projMat;
+	projMat = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
 
 	//渲染循环
 	while(!glfwWindowShouldClose(window)){
-		processInput(window);//检查esc
+		processInput(window);//检查esc     
 
+		//旋转盒子
+		modelMat = glm::rotate(modelMat, (float)glfwGetTime() * glm::radians(50.0f), glm::vec3(0.5f, 1.0f, 0.0f));
 		// 渲染背景颜色
 		glClearColor(0.1f, 0.6f, 0.8f, 0.1f);
-		glClear(GL_COLOR_BUFFER_BIT);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 		//画三角形
 		//glDrawArrays(GL_TRIANGLES, 0, 6);
-		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+		glBindTexture(GL_TEXTURE_2D, TexBufferA);
+		glBindTexture(GL_TEXTURE_2D, TexBufferB);
 
-		////uniform
+		glBindVertexArray(VAO);
+		//glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+
+		
+		//十次drawCall
+		for (int i = 0; i < 10; i++)
+		{
+			glm::mat4 modelMat2;
+			modelMat2 = glm::translate(modelMat2, cubePositions[i]);
+			modelMat2 = glm::rotate(modelMat2, (float)glfwGetTime() * glm::radians(50.0f), glm::vec3(0.5f, 1.0f, 0.0f));
+
+			////uniform
 		//float timeValue = glfwGetTime();
 		//float greenValue = (sin(timeValue) / 2.0f) + 0.5f;
 		//int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
 		//glBindVertexArray(VAO);
 		//glUseProgram(shaderProgram);
 		//glUniform4f(vertexColorLocation, 0, greenValue, 0, 1.0f);
-		myShader->use();
+			myShader->use();
+			glUniform1i(glGetUniformLocation(myShader->ID, "ourTexture"), 0);
+			glUniform1i(glGetUniformLocation(myShader->ID, "ourFace"), 3);
+			//glUniformMatrix4fv(glGetUniformLocation(myShader->ID,"transform"), 1, GL_FALSE,glm::value_ptr(trans));
+			glUniformMatrix4fv(glGetUniformLocation(myShader->ID, "modelMat"), 1, GL_FALSE, glm::value_ptr(modelMat2));
+			glUniformMatrix4fv(glGetUniformLocation(myShader->ID, "viewMat"), 1, GL_FALSE, glm::value_ptr(viewMat));
+			glUniformMatrix4fv(glGetUniformLocation(myShader->ID, "projMat"), 1, GL_FALSE, glm::value_ptr(projMat));
 
-		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+			glDrawArrays(GL_TRIANGLES, 0, 36);
+			//glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		}
+
+
+		
 
 		glfwSwapBuffers(window);//交换前后缓存
 		glfwPollEvents();//监听事件
